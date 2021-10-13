b4b2676739571ea48463ae56ac1546495a862288
Mason-lackhyun <81288975+Mason-lackhyun@users.noreply.github.com>
2021-10-13 19:40:42 +0900

    update

--- 3.SC.tf
+++ 3.SC.tf
@@ -1,5 +1,9 @@
+
 resource "aws_security_group" "ssh" {
   vpc_id = "${aws_vpc.dev.id}"
+  tags = {
+    Name    = "Ext to GW"
+   }
   name = "DEV-SG"
   ingress {
     from_port = 22
@@ -22,6 +26,27 @@ resource "aws_security_group" "ssh" {
   }
 
   egress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_security_group" "ec2" {
+  vpc_id = "${aws_vpc.dev.id}"
+  tags = {
+    Name    = "GW to EC2"
+   }
+  name = "GW to EC2"
+  ingress {
+    from_port = 22
+    to_port = 22
+    protocol = "tcp"
+    security_groups = ["${aws_security_group.ssh.id}"]
+
+  }
+    egress {
     from_port = 0
     to_port = 0
     protocol = "-1"
--- 5.EC2.tf
+++ 5.EC2.tf
@@ -1,17 +1,3 @@
-resource "aws_instance" "mason" {
-  ami             = "ami-08c64544f5cfcddd0"
-  instance_type   = "t2.micro"
-  key_name        = "mason"
-  subnet_id       = "${aws_subnet.sub-a.id}"
-  vpc_security_group_ids = [aws_security_group.ssh.id]
-    root_block_device {
-        volume_size = "20"
-        volume_type = "gp3"
-    }
-    tags = {
-        Name = "Mason"
-    }
-}
 resource "aws_instance" "gw" {
   ami             = "ami-08c64544f5cfcddd0"
   instance_type   = "t2.micro"
@@ -25,5 +11,20 @@ resource "aws_instance" "gw" {
     }
     tags = {
         Name = "GW"
+    }
+}
+
+resource "aws_instance" "mason" {
+  ami             = "ami-08c64544f5cfcddd0"
+  instance_type   = "t2.micro"
+  key_name        = "mason"
+  subnet_id       = "${aws_subnet.sub-c.id}"
+  vpc_security_group_ids = [aws_security_group.ec2.id]
+    root_block_device {
+        volume_size = "20"
+        volume_type = "gp3"
+    }
+    tags = {
+        Name = "Mason"
     }
 }
