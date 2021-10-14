bfbff6cb408c8e910e994203748f1f64d50fe550
Mason-lackhyun <81288975+Mason-lackhyun@users.noreply.github.com>
2021-10-14 19:52:17 +0900

    add ec2:cherry

--- 5.EC2.tf
+++ 5.EC2.tf
@@ -29,3 +29,19 @@ resource "aws_instance" "mason" {
         Name = "Mason"
     }
 }
+## EC2 "cherry" 생성
+resource "aws_instance" "cherry" {
+  ami             = "ami-08c64544f5cfcddd0"
+  instance_type   = "t2.micro"
+  key_name        = "mason"
+  subnet_id       = "${aws_subnet.sub-a.id}"
+  vpc_security_group_ids = [aws_security_group.ssh.id]
+  associate_public_ip_address = true
+    root_block_device {
+        volume_size = "20"
+        volume_type = "gp3"
+    }
+    tags = {
+        Name = "cherry"
+    }
+}
