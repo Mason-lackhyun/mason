94323c87c8fba081c81fa4a8857adb34064c01e2
Mason-lackhyun <81288975+Mason-lackhyun@users.noreply.github.com>
2022-06-27 03:30:08 +0900

    Create test.tf

---
+++ test/test.tf
@@ -0,0 +1,24 @@
+resource "aws_instance" "test1" {
+  ami             = "data.aws_ami.linux.id"
+  instance_type   = "t2.micro"
+  key_name        = "mason"
+  
+
+    root_block_device {
+        volume_size = "20"
+        volume_type = "gp3"
+    }
+    tags = {
+        Name = "test1"
+    }
+}
+
+  data "aws_ami" "linux" {
+  owners      = ["amazon"]
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm*"]
+  }
+}
